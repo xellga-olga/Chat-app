5188b27e283b9454f3cb8b2226221896b6828603
Noskina Olha <noskinaolga1@gmail.com>
2024-05-30 20:52:18 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors({
+   origin: process.env.FRONTEND_URL,
+   credentials: true
+}));
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (request, response) => {
+   response.json({
+      message: 'Server running at ' + PORT
+   });
+});
+
+app.listen(PORT, () => {
+   console.log('server running at ' + PORT);
+});
