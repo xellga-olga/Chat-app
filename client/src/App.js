4496d0c746436c4663810b9cd853b02958846b3f
Noskina Olha <noskinaolga1@gmail.com>
2024-06-16 17:58:57 +0200

    pages extension

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
