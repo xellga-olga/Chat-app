8f114abc4ab22cc93a5ecd8786d41b36339500c7
Noskina Olha <noskinaolga1@gmail.com>
2024-06-18 14:26:49 +0200

    added pages/user authorization/after entering email and password go to page

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,62 +1,66 @@
-import React from 'react'
+import React from "react";
+import { PiUserCircle } from "react-icons/pi";
+import { useSelector } from "react-redux";
 
-import { TbUserCircle } from "react-icons/tb";
+const Avatar = ({ userId, name, imageUrl, width, height }) => {
+  const onlineUser = useSelector((state) => state?.user?.onlineUser);
 
-const Avatar = ({ user, name, imageUrl, width, heigth }) => {
+  let avatarName = "";
 
-   let avatarName = ""
+  if (name) {
+    const splitName = name?.split(" ");
 
-   if(name) {
-      const splitName = name?.split(" ")
+    if (splitName.length > 1) {
+      avatarName = splitName[0][0] + splitName[1][0];
+    } else {
+      avatarName = splitName[0][0];
+    }
+  }
 
-      if(splitName.length > 1) {
-         avatarName = splitName[0][0] + splitName[1][0]
-      } else {
-         avatarName = splitName[0][0]
-      }
-   }
+  const bgColor = [
+    "bg-slate-200",
+    "bg-teal-200",
+    "bg-red-200",
+    "bg-green-200",
+    "bg-yellow-200",
+    "bg-gray-200",
+    "bg-cyan-200",
+    "bg-sky-200",
+    "bg-blue-200",
+  ];
 
-   const bgColor = [
-      'bg-slate-200',
-      'bg-teal-200',
-      'bg-red-200',
-      'bg-blue-200',
-      'bg-indigo-200',
-      'bg-green-200',
-      'bg-yellow-200',
-      'bg-gray-200',
-      'bg-amber-200',
-      'bg-lime-200',
-      'bg-emerald-200',
-      'bg-cyan-200',
-      'bg-sky-200'
-   ]
+  const randomNumber = Math.floor(Math.random() * 9);
 
-   const randomNumber = Math.floor(Math.random() * 13)
-
-   console.log(randomNumber)
-
+  const isOnline = onlineUser.includes(userId);
   return (
-    <div className={`text-slate-800 overflow-hidden rounded-full font-bold `} style={{width : width+"px", height : heigth+"px"}}  >
-      {
-         imageUrl ? (
-            <img 
-               src={imageUrl}
-               width={width}
-               height={heigth}
-               alt={name}
-               className='overflow-hidden rounded-full '
-            />
-         ) : (
-            name ? (
-               <div style={{width : width+"px", height : heigth+"px"}} className={`flex justify-center text-lg items-center overflow-hidden rounded-full ${bgColor[randomNumber]}`}>{avatarName}</div>
-            ) : (
-               <TbUserCircle size={width}/>
-            )
-         )
-      }
-    </div>
-  )
-}
+    <div
+      className={`text-slate-800  rounded-full font-bold relative`}
+      style={{ width: width + "px", height: height + "px" }}
+    >
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          width={width}
+          height={height}
+          alt={name}
+          className="overflow-hidden rounded-full"
+        />
+      ) : name ? (
+        <div
+          style={{ width: width + "px", height: height + "px" }}
+          className={`overflow-hidden rounded-full flex justify-center items-center text-lg ${bgColor[randomNumber]}`}
+        >
+          {avatarName}
+        </div>
+      ) : (
+        <PiUserCircle size={width} />
+      )}
 
-export default Avatar
+      {isOnline && (
+        <div className="bg-green-600 p-1 absolute bottom-2 -right-1 z-10 rounded-full"></div>
+      )}
+    </div>
+  );
+};
+
+export default Avatar;
